27efca2185215ccc8f998926858c01f53958e426
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2023-04-18 00:42:44 +0300

    feat: add solution for "vigenere-cipher"

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,18 +19,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
+// ********** DONE **********
+
 class VigenereCipheringMachine {
-  constructor(bool) {
+  constructor(bool = true) {
     this.bool = bool
   }
 
   encrypt(message, key) {
-    if(!message || !key) throw Error('Invalid date!')
+    if(!message || !key) throw Error('Incorrect arguments!')
 
-
+    return this.handleCrypt(message, key, 'encrypt')
   }
   decrypt(encryptedMessage, key) {
-    if(!encryptedMessage || !key) throw Error('Invalid date!')
+    if(!encryptedMessage || !key) throw Error('Incorrect arguments!')
+
+    return this.handleCrypt(encryptedMessage, key, 'decrypt')
+  }
+
+  handleCrypt(msg, key, type) {
+    // const codes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const secretMsg = msg.toUpperCase()
+    const secretKey = key.toUpperCase()
+    const arr = secretMsg.split('')
+
+    let result = [];
+    let step = 0;
+
+    arr.forEach((item, index) => {
+        const charValue = secretMsg.charCodeAt(index);
+        const isValid = charValue >= 65 && charValue <= 90 
+
+        if (isValid) {
+          const keyInCode = secretKey.charCodeAt(step % secretKey.length) - 65;
+          const code = (type === 'encrypt') 
+            ? (((charValue - 65) + keyInCode) % 26) + 65
+            : (((charValue - 65) - keyInCode + 26) % 26) + 65
+          result.push(String.fromCharCode(code));
+          step++;
+        } else {
+          result.push(item);
+        }
+      })
+
+    // console.log(result.join(""));
+    return this.bool ? result.join("") : result.reverse().join("");
   }
 }
 
